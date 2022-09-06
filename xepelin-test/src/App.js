761caa881ea5654a5085bb0acf3f6277554dd1e2
belfuster <belen.fuster@forcast.cl>
2022-09-06 17:04:37 -0400

    :art: changed to use react router

--- xepelin-test/src/App.js
+++ xepelin-test/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Login from './pages/Login';
+import Greet from './components/Greet';
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Navigator from './components/Navigator';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigator/>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
