013f5acee5716a7a65ac451fe96d453b40bfc036
belfuster <belen.fuster@forcast.cl>
2022-09-08 13:14:40 -0400

    :construction: changes for connecting with a backend not finished

--- xepelin-test/src/App.js
+++ xepelin-test/src/App.js
@@ -1,23 +1,34 @@
-// import logo from './logo.svg';
 import './App.css';
-// import Login from './pages/Login';
-import Greet from './components/Greet';
+import * as React from 'react';
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import Landing from './pages/Landing';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
-import Navigator from './components/Navigator';
+import NavBar from './components/NavBar';
+import MainPage from './pages/MainPage';
 
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/testAPI")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.message)
+        console.log(data.message)
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Navigator/>
+      <NavBar/>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/' element={<Landing />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/main' element={<MainPage />}></Route>
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
